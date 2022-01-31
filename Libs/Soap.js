04eb48e4867c5a416b9b8799b28aa52d85ddf762
burkanakyurek <burkanakyurek@hotmail.com>
2022-01-31 22:56:10 +0300

    updated xml urls and options headers

--- Libs/Soap.js
+++ Libs/Soap.js
@@ -1,45 +1,6 @@
 
-
 var request = require('request');
 var xml2js = require('xml2js');
-
-
-/*
-let xml =
-    `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:qtre="http://Main.Service">
-   <soapenv:Header/>
-   <soapenv:Body>
-      <qtre:GetUsers>
-         <qtre:sSearchText></qtre:sSearchText>
-      </qtre:GetUsers>
-   </soapenv:Body>
-</soapenv:Envelope>`
-
-var options = {
-    url: 'http://test-dmz.ew.com.tr:8080/turkpos.ws/service_turkpos_test.asmx',
-    method: 'POST',
-    body: xml,
-    headers: {
-        'Content-Type': 'text/xml;charset=utf-8',
-        'Accept-Encoding': 'gzip,deflate',
-        'Content-Length': xml.length,
-        'SOAPAction': "http://test-dmz.ew.com.tr:8080/turkpos.ws/service_turkpos_test.asmx"
-    }
-};
-
-let callback = (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        console.log('Raw result', body);
-        var xml2js = require('xml2js');
-        var parser = new xml2js.Parser({ explicitArray: false, trim: true });
-        parser.parseString(body, (err, result) => {
-            console.log('JSON result', result);
-        });
-    };
-    console.log('E', response.statusCode, response.statusMessage);
-};
-request(options, callback);
-*/
 
 function createHashSecurityKey(url, securityString) {
     let xml = `<x:Envelope xmlns:x="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tur="https://turkpos.com.tr/">
@@ -68,6 +29,7 @@ function createHashSecurityKey(url, securityString) {
                 parser.parseString(body, (err, result) => {
                     try {
                         const myHash = result['soap:Envelope']['soap:Body']['SHA2B64Response']['SHA2B64Result']
+                        console.log(myHash)
                         return resolve(myHash)
                     } catch (error) {
                         return reject(error)
@@ -102,29 +64,9 @@ function setPaid(url, {
     securityType,
     ipAddress,
 }) {
+    console.log(createdHash + "last hash")
 
 
-    // let {
-    //     CLIENT_CODE,
-    //     CLIENT_USERNAME,
-    //     CLIENT_PASSWORD,
-    //     GUID,
-    //     cardName,
-    //     cardNumber,
-    //     cardExpmonth,
-    //     cardExpyear,
-    //     cardCvv,
-    //     cardHolderPhone,
-    //     failUrl,
-    //     successUrl,
-    //     orderId,
-    //     installment,
-    //     description,
-    //     total,
-    //     createdHash,
-    //     securityType,
-    //     ipAddress,
-    // } = args
     let xml = `<?xml version="1.0" encoding="utf-8"?> <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
     <soap:Body>
     <Pos_Odeme xmlns="https://turkpos.com.tr/">
@@ -136,7 +78,7 @@ function setPaid(url, {
     <GUID>${GUID}</GUID>
     <KK_Sahibi>${cardName}</KK_Sahibi>
     <KK_No>${cardNumber}</KK_No>
-    <KK_SK_Ay>${cardExpmonth}'</KK_SK_Ay>
+    <KK_SK_Ay>${cardExpmonth}</KK_SK_Ay>
     <KK_SK_Yil>${cardExpyear}</KK_SK_Yil>
     <KK_CVC>${cardCvv}</KK_CVC>
     <KK_Sahibi_GSM>${cardHolderPhone}</KK_Sahibi_GSM>
@@ -149,40 +91,41 @@ function setPaid(url, {
     <Toplam_Tutar>${total}</Toplam_Tutar>
     <Islem_Hash>${createdHash}</Islem_Hash>
     <Islem_Guvenlik_Tip>${securityType}</Islem_Guvenlik_Tip>
-    <Islem_ID></Islem_ID>
+    <Islem_ID>sipariş1</Islem_ID>
     <IPAdr>${ipAddress}</IPAdr>
-    <Ref_URL></Ref_URL>
-    <Data1></Data1>
-    <Data2></Data2>
-    <Data3></Data3>
-    <Data4></Data4>
-    <Data5></Data5>
-    <Data6></Data6>
-    <Data7></Data7>
-    <Data8></Data8>
-    <Data9></Data9>
-    <Data10></Data10>
+    <Ref_URL>www.ornekornek.com</Ref_URL>
+    <Data1>123</Data1>
+    <Data2>456</Data2>
+    <Data3>789</Data3>
+    <Data4>string</Data4>
+    <Data5>string</Data5>
+    <Data6>string</Data6>
+    <Data7>string</Data7>
+    <Data8>string</Data8>
+    <Data9>string</Data9>
+    <Data10>string</Data10>
     </Pos_Odeme>
     </soap:Body>
     </soap:Envelope>`
-    var options = {
+
+    let options = {
         url,
         method: 'POST',
         body: xml,
         headers: {
-            'Content-Type': 'text/xml;charset=utf-8',
-            'Accept-Encoding': 'gzip,deflate',
-            'Content-Length': xml.length
+            'Content-Type': "text/xml",
         }
     };
-
+    
     return new Promise((resolve, reject) => {
+
         request(options, (error, response, body) => {
-            // console.log('JSON response', response);
+            console.log('JSON response', response);
+
             if (!error && response.statusCode == 200) {
                 var parser = new xml2js.Parser({ explicitArray: false, trim: true });
                 parser.parseString(body, (err, result) => {
-                    //console.log('JSON result', result);
+                    console.log('JSON result', result);
                     try {
                         const resultFin = {
                             success: result['soap:Envelope']['soap:Body']['Pos_OdemeResponse']['Pos_OdemeResult']['Islem_ID'] == '0' ? false : true,
@@ -193,6 +136,7 @@ function setPaid(url, {
                         }
                         return resolve(resultFin)
                     } catch (err) {
+                        console.log(err)
                         return reject(err)
                     }
                 });
@@ -202,8 +146,6 @@ function setPaid(url, {
             }
         })
     })
-
-
 }
 
 module.exports = {
